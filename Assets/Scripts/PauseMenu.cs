dd1213d656c088e516db7eb1a90103199409acfd
Nermin Topalovic <nerminto@edu.aau.at>
2023-06-03 21:23:29 +0200

    die (sytem)

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,10 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pausePanel;
+    public GameObject gameOverPanel;
+    public int currentSceneIndex=1;
     
     // Start is called before the first frame update
     void Start()
@@ -15,22 +19,35 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Escape))
+        if (Input.GetKey(KeyCode.Escape) && !gameOverPanel.activeSelf)
         {
             Pause();
         }
     }
 
-    public void Pause()
+    private void Pause()
     {
         Time.timeScale = 0;
         pausePanel.gameObject.SetActive(true);
+    }
+
+    public void GameOver()
+    {
+        Time.timeScale = 0;
+        gameOverPanel.gameObject.SetActive(true);
     }
 
     public void Continue()
     {
         Time.timeScale = 1;
         pausePanel.gameObject.SetActive(false);
+    }
+
+    public void Restart()
+    {
+        //restart
+        Time.timeScale = 1;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 
     public void Exit()
---
+++ Assets/Scripts/Player/Health.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class Health : MonoBehaviour
+    {
+        public int currentHealth = 4;
+        public HealthBar bar;
+        public PauseMenu pauseMenu;
+        public float maxInvincibleTime = 1f;
+        private float currentInvincibleTIme;
+        
+        public void GetDamaged()
+        {
+            if (currentInvincibleTIme > 0)
+            {
+                return;
+            }
+            currentHealth--;
+            currentInvincibleTIme = maxInvincibleTime;
+            bar.SetHealthBar(currentHealth);
+            if (currentHealth <= 0)
+            {
+                //game over
+                pauseMenu.GameOver();
+            }
+        }
+
+        private void Update()
+        {
+            if (currentInvincibleTIme > 0)
+            {
+                currentInvincibleTIme -= Time.deltaTime;  
+            }
+            
+        }
+
+        
+    }
+}
